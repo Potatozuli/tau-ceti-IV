5fe82878c21dfae27911c3086566ab389ce298bb
Potatozuli <odin.taylor@me.com>
2023-03-27 22:57:29 -0500

    upda

--- scripts/tcivDeAimer.js
+++ scripts/tcivDeAimer.js
@@ -2,8 +2,8 @@ Hooks.once("preUpdateToken", () => { //Movement de-aiming
     const lastPosX = token.x;
     const lastPosY = token.y;
     let freeMove = 1;
-    if(token.actor.system.props.Mobile == 1 && token.actor.system.props.Agility > 1){
-        freeMove = token.actor.system.props.Agility;
+    if(token.actor.system.props.Mobile == 1 && Number(token.actor.system.props.Agility) > 1){
+        freeMove = Number(token.actor.system.props.Agility);
     }
     Hooks.once("updateToken", () => {
         distMoved = Math.floor(Math.sqrt(Math.pow((lastPosX - token.x)/canvas.grid.size, 2) + Math.pow((lastPosY - token.y)/canvas.grid.size, 2))) - 1;
--- scripts/tcivSpeedExtender.js
+++ scripts/tcivSpeedExtender.js
@@ -11,15 +11,15 @@ Hooks.once("dragRuler.ready", (SpeedProvider) => {
         getRanges(token) {
             const baseSpeed = token.actor.system.props.Speed
             let freeMove = 1;
-            if(token.actor.system.props.Mobile == 1 && token.actor.system.props.Agility > 1){
-                freeMove = token.actor.system.props.Agility;
+            if(token.actor.system.props.Mobile == 1 && Number(token.actor.system.props.Agility) > 1){
+                freeMove = Number(token.actor.system.props.Agility);
             }
 
 			// A character can always walk it's base speed and dash twice it's base speed
 			const ranges = [
 				{range: freeMove, color: "Free"},
-				{range: baseSpeed + freeMove, color: "Move"},
-                {range: baseSpeed*2 + freeMove, color: "Movex2"}
+				{range: (baseSpeed + freeMove), color: "Move"},
+                {range: (baseSpeed*2 + freeMove), color: "Movex2"}
 			]
 
             return ranges
