5677e84de725016ab82b6ae1d6210852645786bc
Potatozuli <odin.taylor@me.com>
2023-03-27 23:08:28 -0500

    we

--- scripts/tcivDeAimer.js
+++ scripts/tcivDeAimer.js
@@ -1,12 +1,14 @@
 Hooks.once("preUpdateToken", () => { //Movement de-aiming
     const lastPosX = token.x;
     const lastPosY = token.y;
+    console.log("Beep got positions")
+    console.log(lastPosX)
     let freeMove = 1;
     if(token.actor.system.props.Mobile == 1 && Number(token.actor.system.props.Agility) > 1){
         freeMove = Number(token.actor.system.props.Agility);
     }
     Hooks.once("updateToken", () => {
-        distMoved = Math.floor(Math.sqrt(Math.pow((lastPosX - token.x)/canvas.grid.size, 2) + Math.pow((lastPosY - token.y)/canvas.grid.size, 2))) - 1;
+        const distMoved = Math.floor(Math.sqrt(Math.pow((lastPosX - token.x)/canvas.grid.size, 2) + Math.pow((lastPosY - token.y)/canvas.grid.size, 2))) - 1;
         if(distMoved > freeMove){
             token.actor.update({"system.props.IsAimed": 0}); //De-aims player if they move more than free movement
         }
