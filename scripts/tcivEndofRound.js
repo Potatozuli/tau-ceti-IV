4d16e4faa270eac60a9fb29b44bd4d28e944b4b4
Potatozuli <odin.taylor@me.com>
2023-03-29 13:59:18 -0500

    gamer

--- scripts/tcivEndofRound.js
+++ scripts/tcivEndofRound.js
@@ -1,17 +1,17 @@
 Hooks.on("combatRound", () => { //Updates for the start of combat rounds
     for (changedToken of canvas.tokens.placeables){
         if(changedToken.actor.system.props.Stunned == 1 && Number(changedToken.actor.system.props.Current_Health) > 0){
-            checkedToken.actor.update({"system.props.Stunned": 0});
-            checkedtoken.toggleEffect("icons/svg/blind.svg", {active: false, overlay: true});
+            changedToken.actor.update({"system.props.Stunned": 0});
+            changedToken.toggleEffect("icons/svg/blind.svg", {active: false, overlay: true});
         }
         if(changedToken.actor.system.props.EnteredFire == 1){
-            checkedToken.actor.update({"system.props.EnteredFire": 0});
+            changedToken.actor.update({"system.props.EnteredFire": 0});
         }
     }
     for(let checkedTemplate of canvas.scene.templates){ //For all templates on scene
         if(Tagger.hasTags(checkedTemplate, "Smoke, Fire", {matchAny: true})){ //For templates with either Smoke or Fire tag
             let tags = Tagger.getTags(checkedTemplate); //Get tags
-            let roundsLeft = tags[1].replace(/\D/g, ''); //Find rounds left in second tag
+            let roundsLeft = tags[1].replace(/\D/g, ""); //Find rounds left in second tag
             Tagger.removeTags(checkedTemplate, `RoundsLeft: ${roundsLeft}`); //Removes previous RoundsLeft tag
             roundsLeft--
             if(roundsLeft == 0){
