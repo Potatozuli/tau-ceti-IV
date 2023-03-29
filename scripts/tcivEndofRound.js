90a158c979aa5a82a3a6ac28b5cf465146d9b1a0
Potatozuli <odin.taylor@me.com>
2023-03-29 00:14:16 -0500

    e

--- scripts/tcivEndofRound.js
+++ scripts/tcivEndofRound.js
@@ -1,6 +1,7 @@
 Hooks.on("combatRound", () => { //Updates for the start of combat rounds
     console.log(canvas.tokens.placeables);
     for (changedToken of canvas.tokens.placeables){
+        console.log("Beep 1");
         if(changedToken.actor.Stunned == 1 && Number(changedToken.actor.Current_Health) > 0){
             console.log("beep");
             checkedToken.actor.update({"system.props.Stunned": 0});
