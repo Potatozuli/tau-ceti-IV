f948643ed9756f7a3c181a6d2d808a1526fac28c
Potatozuli <odin.taylor@me.com>
2023-03-29 00:34:16 -0500

    a

--- scripts/tcivEndofRound.js
+++ scripts/tcivEndofRound.js
@@ -1,13 +1,11 @@
 Hooks.on("combatRound", () => { //Updates for the start of combat rounds
     console.log(canvas.tokens.placeables);
     for (changedToken of canvas.tokens.placeables){
-        console.log("Beep 1");
-        if(changedToken.actor.Stunned == 1 && Number(changedToken.actor.Current_Health) > 0){
-            console.log("beep");
+        if(changedToken.actor.system.props.Stunned == 1 && Number(changedToken.actor.system.props.Current_Health) > 0){
             checkedToken.actor.update({"system.props.Stunned": 0});
             checkedtoken.toggleEffect("icons/svg/blind.svg", {active: false, overlay: true});
         }
-        if(changedToken.actor.EnteredFire == 1){
+        if(changedToken.actor.system.props.EnteredFire == 1){
             checkedToken.actor.update({"system.props.EnteredFire": 0});
         }
     }
