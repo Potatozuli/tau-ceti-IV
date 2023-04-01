d7455869b11c406c3fc0386b059ae11a2536b52b
Potatozuli <odin.taylor@me.com>
2023-03-31 20:12:20 -0500

    ga

--- scripts/tcivEndofRound.js
+++ scripts/tcivEndofRound.js
@@ -1,4 +1,4 @@
-Hooks.on("combatRound", () => { //Updates for the start of combat rounds
+Hooks.once("combatRound", () => { //Updates for the start of combat rounds
     for (changedToken of canvas.tokens.placeables){
         if(changedToken.actor.system.props.Stunned == 1 && Number(changedToken.actor.system.props.Current_Health) > 0){
             changedToken.actor.update({"system.props.Stunned": 0});
@@ -11,14 +11,18 @@ Hooks.on("combatRound", () => { //Updates for the start of combat rounds
     for(let checkedTemplate of canvas.scene.templates){ //For all templates on scene
         if(Tagger.hasTags(checkedTemplate, "Smoke, Fire", {matchAny: true})){ //For templates with either Smoke or Fire tag
             let tags = Tagger.getTags(checkedTemplate); //Get tags
+            console.log(tags)
             let roundsLeft = tags[1].replace(/\D/g, ""); //Find rounds left in second tag
+            console.log(roundsLeft)
             Tagger.removeTags(checkedTemplate, `RoundsLeft: ${roundsLeft}`); //Removes previous RoundsLeft tag
-            roundsLeft--
+            console.log(Tagger.getTags(checkedTemplate))
+            roundsLeft--;
             if(roundsLeft == 0){
                 canvas.scene.deleteEmbeddedDocuments("MeasuredTemplate", [checkedTemplate.id]); //Deletes template if out of time
             } else {
                 Tagger.addTags(checkedTemplate, `RoundsLeft: ${roundsLeft}`);
             }
+            console.log(Tagger.getTags(checkedTemplate))
         }
     }
 })
