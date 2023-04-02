0b1e0bbad21015c4ed31664d09a71f06fcb949e2
Potatozuli <odin.taylor@me.com>
2023-04-02 13:54:18 -0500

    asedf

---
+++ scripts/tcivSpeedExtender.mjs
@@ -0,0 +1,30 @@
+Hooks.once("dragRuler.ready", (SpeedProvider) => {
+    class tcIVSpeedExtender extends SpeedProvider {
+        get colors() {
+            return [
+                {id: "Free", default: 0x00FF00, name: "tcIV.speeds.Free"},
+                {id: "Move", default: 0xFFFF00, name: "tcIV.speeds.Move"},
+                {id: "Movex2", default: 0xFF8000, name: "tcIV.speeds.Movex2"}
+            ]
+        }
+
+        getRanges(token) {
+            const baseSpeed = token.actor.system.props.Speed
+            let freeMove = 1;
+            if(token.actor.system.props.Mobile == 1 && Number(token.actor.system.props.Agility) > 1){
+                freeMove = Number(token.actor.system.props.Agility);
+            }
+
+			// A character can always walk it's base speed and dash twice it's base speed
+			const ranges = [
+				{range: freeMove, color: "Free"},
+				{range: (baseSpeed + freeMove), color: "Move"},
+                {range: (baseSpeed*2 + freeMove), color: "Movex2"}
+			]
+
+            return ranges
+        }
+    }
+
+    dragRuler.registerModule("tau-ceti-iv", tcIVSpeedExtender)
+})
