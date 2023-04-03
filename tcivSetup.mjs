af5982417dfee0383357612ff0d51f21dc000546
Potatozuli <odin.taylor@me.com>
2023-04-03 17:46:45 -0500

    ga

--- tcivSetup.mjs
+++ tcivSetup.mjs
@@ -1,9 +1,16 @@
 import {
+    tcivAttackA
+} from "./functions/tcivAttackA.mjs";
+
+import {
     tcivAttackB
 } from "./functions/tcivAttackB.mjs";
 
 Hooks.once("setup", () => {
     console.log("Tau Ceti IV Initializing")
     console.log(tcivAttackB)
-    game.modules.get("taucetiiv").api = tcivAttackB
+    game.modules.get("taucetiiv").api = { 
+        tcivAttackA,
+        tcivAttackB
+    }
 });
