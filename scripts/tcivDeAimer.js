32557806362d0bc4e6b1428e2756880290ecbdff
Potatozuli <odin.taylor@me.com>
2023-06-09 01:13:19 -0500

    gamer2

--- scripts/tcivDeAimer.js
+++ scripts/tcivDeAimer.js
@@ -33,7 +33,7 @@ Hooks.on("preUpdateToken", (token, updates) => { //Movement de-aiming
         const distMoved = Math.floor(Math.hypot(token.x - updates.x, token.y - updates.y)/canvas.grid.size);
         if(distMoved > freeMove){
             token.actor.update({"system.props.IsAimed": 0}); //De-aims player if they move more than free movement from last aimed point
-            if(token.actor.system.props.Aimed_Target) {
+            if(canvas.scene.tokens.get(token.actor.system.props.Aimed_Target) !== undefined){
                 warpgate.mutate(canvas.tokens.get(token.actor.system.props.Aimed_Target).document, {token: {overlayEffect: ""}}, {permanent: true, alwaysAccept: true}); //Removes aimed symbol from previous target
             }
             ui.notifications.info("Lost aim from movement!");
