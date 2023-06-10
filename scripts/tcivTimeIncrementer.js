193f402d3dcadb16e642fe0697f6fa1aaccc90bb
Potatozuli <odin.taylor@me.com>
2023-06-10 17:25:37 -0500

    awef

---
+++ scripts/tcivTimeIncrementer.js
@@ -0,0 +1,7 @@
+// Hooks.on("preUpdateToken", (token, updates) => { //Movement de-aiming
+//     if((updates.x ?? updates.y) && (token.actor.system.props.Pace !== undefined)){ //If the player has moved
+
+//         speed = Number(token.actor.system.props.Speed);
+//         const distMoved = Math.floor(Math.hypot(token.x - updates.x, token.y - updates.y)/canvas.grid.size);
+//     }
+// });
