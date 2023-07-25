a7850454b3bd2f9af87e3cbbd4e18dd30305ce05
Potatozuli <odin.taylor@me.com>
2023-07-25 10:22:50 -0500

    gawef

--- scripts/tcivSpeedExtender.js
+++ scripts/tcivSpeedExtender.js
@@ -2,7 +2,7 @@ Hooks.once("dragRuler.ready", (SpeedProvider) => {
     class tcIVSpeedExtender extends SpeedProvider {
         get colors() {
             return [
-                {id: "Free", default: 0x00FF00, name: "tcIV.speeds.Free"},
+                //{id: "Free", default: 0x00FF00, name: "tcIV.speeds.Free"},
                 {id: "Move", default: 0xFFFF00, name: "tcIV.speeds.Move"},
                 {id: "Movex2", default: 0xFF8000, name: "tcIV.speeds.Movex2"}
             ]
@@ -10,15 +10,15 @@ Hooks.once("dragRuler.ready", (SpeedProvider) => {
 
         getRanges(token) {
             const baseSpeed = Number(token.actor.system.props.Speed)
-            let freeMove = 1;
-            if(token.actor.system.props.Mobile == 1 && Number(token.actor.system.props.Agility) > 1){
-                freeMove = Number(token.actor.system.props.Agility);
-            }
+            //let freeMove = 1;
+            //if(token.actor.system.props.Mobile == 1 && Number(token.actor.system.props.Agility) > 1){
+            //    freeMove = Number(token.actor.system.props.Agility);
+            //}
 
 			const ranges = [
-				{range: freeMove, color: "Free"},
-				{range: (baseSpeed + freeMove), color: "Move"},
-                {range: (baseSpeed*2 + freeMove), color: "Movex2"}
+				//{range: freeMove, color: "Free"},
+				{range: (baseSpeed), color: "Move"},
+                {range: (baseSpeed*2), color: "Movex2"}
 			]
 
             return ranges
